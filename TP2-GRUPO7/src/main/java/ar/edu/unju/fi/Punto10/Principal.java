06f6bb0727dd7bad5b47f39e788034956a6d9278
CarlosSejas <ocarl_14@hotmail.com>
2022-04-14 15:02:33 -0300

    punto 10

---
+++ TP2-GRUPO7/src/main/java/ar/edu/unju/fi/Punto10/Principal.java
@@ -0,0 +1,12 @@
+package ar.edu.unju.fi.Punto10;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		int i=320;
+		while(i>=160) {
+			System.out.print(i+" ");
+			i=i-20;
+		}
+	}
+}
