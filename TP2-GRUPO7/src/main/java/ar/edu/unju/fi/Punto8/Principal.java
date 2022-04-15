cefde8cbeb2acf4166e077256b239b166f82cc65
ELIAs <ELIAs@DESKTOP-7DD2KDS>
2022-04-15 16:59:10 -0300

    Punto 8

---
+++ TP2-GRUPO7/src/main/java/ar/edu/unju/fi/Punto8/Principal.java
@@ -0,0 +1,15 @@
+package ar.edu.unju.fi.Punto8;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num=4;	
+		System.out.println("******Secuencia numerica multiplos de 4******");
+		do {
+			System.out.print(num+" ");
+			num = num +4;
+		}while(num<=100);
+	}
+
+}
