26dba2b540686b0299c35af0be1c7b0a5f567409
Carlos PC <Carlos PC@DESKTOP-DHISPFF>
2022-04-16 11:56:21 -0300

    Punto 11

---
+++ TP2-GRUPO7/src/main/java/ar/edu/unju/fi/Punto11/Principal.java
@@ -0,0 +1,22 @@
+package ar.edu.unju.fi.Punto11;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		//valor inicial 320
+		int num = 320;
+		
+		do 
+		{
+			//imprimir
+			System.out.print(num+" ");
+			//restar de 20 en 20
+			num=num-20;
+			
+		}while(num>=160);//solo imprimir hasta 160
+		
+
+	}
+
+}
