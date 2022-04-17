b9c2ccc744231bd6d64f28325347f8aa1398cfd5
patri <patri@DESKTOP-Q73S40N>
2022-04-16 23:25:30 -0300

    punto 7

---
+++ TP2-GRUPO7/src/main/java/ar/edu/unju/fi/Punto7/Principal.java
@@ -0,0 +1,20 @@
+package ar.edu.unju.fi.Punto7;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		int i=2;
+		for (int j=40;j>0;j--)
+		{
+				System.out.print(i*j);
+				System.out.print(" ");
+				i++;
+				
+			}
+			
+		}
+
+
+	}
+
+
